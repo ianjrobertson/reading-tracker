869ade80430608978312ba477481c635474a5425
Ian Robertson <ijr011235@gmail.com>
2025-06-29 22:59:02 -0600

    Protected page navlinks

---
+++ app/protected/leaderboard/page.tsx
@@ -0,0 +1,6 @@
+export default function Leaderboard() {
+
+    return (
+        <div>Coming Soon 🛠️</div>
+    )
+}
--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -1,7 +1,7 @@
 import { redirect } from "next/navigation";
+import Link from "next/link";
 
 import { createClient } from "@/lib/supabase/server";
-import { InfoIcon } from "lucide-react";
 
 export default async function ProtectedPage() {
   const supabase = await createClient();
@@ -13,18 +13,11 @@ export default async function ProtectedPage() {
 
   return (
     <div className="flex-1 w-full flex flex-col gap-12">
-      <div className="w-full">
-        <div className="bg-accent text-sm p-3 px-5 rounded-md text-foreground flex gap-3 items-center">
-          <InfoIcon size="16" strokeWidth={2} />
-          This is a protected page that you can only see as an authenticated
-          user
-        </div>
-      </div>
       <div className="flex flex-col gap-2 items-start">
-        <h2 className="font-bold text-2xl mb-4">Your user details</h2>
-        <pre className="text-xs font-mono p-3 rounded border max-h-32 overflow-auto">
-          {JSON.stringify(data.user, null, 2)}
-        </pre>
+        <h2 className="font-bold text-2xl mb-4">Reading Tracker 📕</h2>
+        <Link href='/protected/add-session'><p className="hover:bg-black hover:text-white rounded p-1">Add a reading session</p></Link>
+        <Link href='/protected/sessions'><p className="hover:bg-black hover:text-white rounded p-1">View my stats</p></Link>
+        <Link href='/protected/leaderboard'><p className="hover:bg-black hover:text-white rounded p-1">View the reading leaderboard</p></Link>
       </div>
       <div>
       </div>
