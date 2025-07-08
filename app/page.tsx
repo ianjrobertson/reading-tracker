c6f9fd36035749b2dc96161e80b03549c77d6b68
Ian Robertson <ijr011235@gmail.com>
2025-07-07 22:34:22 -0600

    refactored main layout to use sidebar

--- app/page.tsx
+++ app/page.tsx
@@ -2,45 +2,48 @@ import { EnvVarWarning } from "@/components/env-var-warning";
 import { AuthButton } from "@/components/auth-button";
 import { ThemeSwitcher } from "@/components/theme-switcher";
 import { hasEnvVars } from "@/lib/utils";
-import Link from "next/link";
+import { Sidebar } from "@/components/sidebar";
 
 export default function Home() {
   return (
-    <main className="min-h-screen flex flex-col items-center">
-      <div className="flex-1 w-full flex flex-col gap-20 items-center">
-        <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-          <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
-            <div className="flex gap-5 items-center font-semibold">
-              <Link href={"/"} className="hover:bg-black hover:text-white dark:hover:bg-white dark:hover:text-black rounded p-1">Reading Tracker 📖</Link>
-              <Link href='/protected/sessions'><p className="hover:bg-black hover:text-white dark:hover:bg-white dark:hover:text-black rounded p-1">My Stats</p></Link>
-              <Link href='/protected/leaderboard'><p className="hover:bg-black hover:text-white dark:hover:bg-white dark:hover:text-black rounded p-1">Leaderboard</p></Link>
-              <Link href='/protected/add-session'><p className="hover:bg-black hover:text-white dark:hover:bg-white dark:hover:text-black rounded p-1">Add +</p></Link>
-            </div>
-            {!hasEnvVars ? <EnvVarWarning /> : <AuthButton />}
-          </div>
-        </nav>
-        <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5">
-          <main className="flex-1 flex flex-col gap-6 px-4">
-            <h2 className="font-medium text-xl mb-4">Welcome to the Reading Tracker App!</h2>
-            <p>Login or create an account to start tracking your reading!</p>
-          </main>
-        </div>
 
-        <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
-          <p>
-            Created by{" "}
-            <a
-              href="https://github.com/ianjrobertson"
-              target="_blank"
-              className="font-bold hover:underline"
-              rel="noreferrer"
-            >
-              Ian Robertson
-            </a>
-          </p>
-          <ThemeSwitcher />
-        </footer>
-      </div>
-    </main>
+    <div className="min-h-screen flex">
+          {/* Sidebar component */}
+          <Sidebar />
+          
+          {/* Main content area */}
+          <div className="flex-1">
+            <main className="min-h-screen flex flex-col items-center">
+              <div className="flex-1 w-full flex flex-col gap-20 items-center">
+                <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
+                  <div className="w-full max-w-5xl flex justify-end items-center p-3 px-5 text-sm">
+                    {!hasEnvVars ? <EnvVarWarning /> : <AuthButton />}
+                  </div>
+                </nav>
+                <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5">
+                  <main className="flex-1 flex flex-col gap-6 px-4">
+                    <h2 className="font-medium text-xl mb-4">Welcome to the Reading Tracker App!</h2>
+                    <p>Login or create an account to start tracking your reading!</p>
+                  </main>
+                </div>
+
+                <footer className="w-full flex items-center justify-center mx-auto text-center text-xs gap-8 py-3">
+                  <p>
+                    Created by{" "}
+                    <a
+                      href="https://github.com/ianjrobertson"
+                      target="_blank"
+                      className="font-bold hover:underline"
+                      rel="noreferrer"
+                    >
+                      Ian Robertson
+                    </a>
+                  </p>
+                  <ThemeSwitcher />
+                  </footer>
+              </div>
+            </main>
+          </div>
+    </div>
   );
 }
